129479f54553c12e1b9bc5eb95d49c533fcf71d3
qafoori <qafoori.hr@gmail.com>
2021-06-30 17:54:25 +0430

    read one row

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'faq', to: 'pages#faq'
+  resources :articles, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_060915) do
+ActiveRecord::Schema.define(version: 2021_06_29_171249) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

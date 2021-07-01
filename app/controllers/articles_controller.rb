5dbea7e6c4c00aab989598acc0825bb9f1199b44
qafoori <qafoori.hr@gmail.com>
2021-07-01 18:08:27 +0430

    crud finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,42 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notic] = "article was successfully created"
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notic] = "Article was successfully updated"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notic] = "Article #{params[:id]} was successfully destroyed"
+    redirect_to articles_path
+  end
+
 end
